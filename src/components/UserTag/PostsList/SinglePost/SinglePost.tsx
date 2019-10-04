abe1ad7848427ae0cd2dec30b2dd4377a2a04bdf
kacluk123 <sakuy124@gmail.com>
2019-10-04 22:33:40 +0200

    icon/styling

--- src/components/UserTag/PostsList/SinglePost/SinglePost.tsx
+++ src/components/UserTag/PostsList/SinglePost/SinglePost.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import * as Styled from "./SinglePostStyles";
 import { SingleUIPostsResponse } from "../../../../api/endpoints/posts/postsTypes";
+import * as Icon from "../../../Common/Icons/Icons";
+import moment from "moment";
 
 const SinglePostComponent = ({
   author,
@@ -11,8 +13,12 @@ const SinglePostComponent = ({
   return (
     <Styled.SinglePost>
       <Styled.SingleUserName>{author}</Styled.SingleUserName>
-      <Styled.SinglePostUserAvatar>avek</Styled.SinglePostUserAvatar>
-      <Styled.SinglePostDate>{createdAt}</Styled.SinglePostDate>
+      <Styled.SinglePostUserAvatar>
+        <Icon.User height="36px" width="36px" />
+      </Styled.SinglePostUserAvatar>
+      <Styled.SinglePostDate>
+        {moment(createdAt).format("MMM Do YY")}
+      </Styled.SinglePostDate>
       <Styled.SinglePostContent>{content}</Styled.SinglePostContent>
     </Styled.SinglePost>
   );
--- src/components/UserTag/PostsList/SinglePost/SinglePostStyles.tsx
+++ src/components/UserTag/PostsList/SinglePost/SinglePostStyles.tsx
@@ -5,35 +5,54 @@ export const SinglePost = styled.div.attrs({
 })`
   width: 100%;
   min-height: 100px;
+  padding: 10px 20px 10px 20px;
   background: #edecea;
   border-radius: 2px;
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-columns: 10% 10% 10% 1fr;
+  grid-template-rows: max-content;
+  grid-row-gap: 10px;
   grid-template-areas:
-    "SinglePostUserAvatar SingleUserName" "SinglePostDate" "SinglePostDate"
-    "SinglePostUserAvatar SinglePostContent" "SinglePostContent" "SinglePostContent";
+    "SinglePostUserAvatar SingleUserName SinglePostDate SinglePostDate"
+    "SinglePostUserAvatar SinglePostContent SinglePostContent SinglePostContent";
 `;
 
 export const SinglePostUserAvatar = styled.div.attrs({
   className: "SinglePostUserAvatar"
 })`
-  grid-area: SinglePostDate;
+  grid-area: SinglePostUserAvatar;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  background: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const SinglePostDate = styled.div.attrs({
   className: "SinglePostDate"
 })`
-  grid-area: SinglePostUserAvatar;
+  grid-area: SinglePostDate;
+  font-size: 12px;
+  font-weight: 400;
+  color: var(--medium-grey);
+  padding-top: 3px;
+  border-bottom: 1px solid var(--tiny-grey);
 `;
 
 export const SinglePostContent = styled.div.attrs({
   className: "SinglePostContent"
 })`
   grid-area: SinglePostContent;
+  word-break: break-all;
 `;
 
 export const SingleUserName = styled.div.attrs({
   className: "SingleUserName"
 })`
   grid-area: SingleUserName;
+  color: var(--red-orange);
+  font-weight: 400;
+  border-bottom: 1px solid var(--tiny-grey);
 `;
